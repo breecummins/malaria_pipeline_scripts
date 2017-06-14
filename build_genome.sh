5fcd5e0570b3866e3efd3668475482fbb08c7f30
Bree Cummins <breecummins@gmail.com>
2017-06-14 17:04:57 -0400

    debugged build_genome. added option to command, and to srun comment

--- build_genome.sh
+++ build_genome.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-# call as: screen -d -m srun --partition=harerlab ./build_genome.sh
-$HOME/programs/STAR/bin/Linux_x86_64/STAR --runThreadN 1 --runMode genomeGenerate --genomeDir /work/bc187/vivax_genome/star_format --genomeFastaFiles /work/bc187/vivax_genome/PlasmoDB-32_PvivaxP01_Genome.fasta --sjdbGTFfile /work/bc187/vivax_genome/PlasmoDB-32_PvivaxP01.gff
+# call as: screen -d -m srun --partition=harerlab --mem=0 ./build_genome.sh
+$HOME/programs/STAR/bin/Linux_x86_64/STAR --runThreadN 1 --runMode genomeGenerate --genomeDir /work/bc187/vivax_genome/star_format --genomeFastaFiles /work/bc187/vivax_genome/PlasmoDB-32_PvivaxP01_Genome.fasta --sjdbGTFfile /work/bc187/vivax_genome/PlasmoDB-32_PvivaxP01.gff --sjdbGTFtagExonParentTranscript Parent
