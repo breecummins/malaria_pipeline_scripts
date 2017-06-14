ce8f9d00e765de449bf3198703307c5d3b3f789d
Bree Cummins <breecummins@gmail.com>
2017-06-14 15:08:32 -0600

    added comments

--- build_genome.sh
+++ build_genome.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
 # call as: screen -d -m srun --partition=harerlab --mem=0 ./build_genome.sh
+# --mem=0 required to access all memory on node
+# option --sjdbGTFtagExonParentTranscript Parent  below needed for .gff files, not .gtf files
+
 $HOME/programs/STAR/bin/Linux_x86_64/STAR --runThreadN 1 --runMode genomeGenerate --genomeDir /work/bc187/vivax_genome/star_format --genomeFastaFiles /work/bc187/vivax_genome/PlasmoDB-32_PvivaxP01_Genome.fasta --sjdbGTFfile /work/bc187/vivax_genome/PlasmoDB-32_PvivaxP01.gff --sjdbGTFtagExonParentTranscript Parent
