10802802ae3b351c98a7d053f069ad9c9e745156
Bree Cummins <breecummins@gmail.com>
2017-07-24 10:54:13 -0600

    added script to rename results

---
+++ scrape_logs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+# call in top level directory -- /work/bc187/malariadataJuly2017/
+
+SAMPLENUM=$1
+SAMPDIR="Sample$SAMPLENUM"
+RESULTSDIR="results_$SAMPDIR"
+
+cd SAMPDIR
+
+for DIR in `ls -d */`
+do
+cd $DIR
+cp ./STAR_out_mal/abundances.cxb ../../$RESULTSDIR/abundances_$DIR_mal.cxb
+cp ./STAR_out_mal/Log.final.out ../../$RESULTSDIR/Log_$DIR_mal.final.out
+cp ./STAR_out_hum/abundances.cxb ../../$RESULTSDIR/abundances_$DIR_hum.cxb
+cp ./STAR_out_hum/Log.final.out ../../$RESULTSDIR/Log_$DIR_hum.final.out
+cd ..
+done
+
