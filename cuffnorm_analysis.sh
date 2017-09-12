f73ace2e3af62536b4d99cf9b0c2fd5866fb1a62
Bree Cummins <breecummins@gmail.com>
2017-09-12 15:16:10 -0600

    finished cuffnorm script

--- cuffnorm_analysis.sh
+++ cuffnorm_analysis.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
 
 ~/programs/cufflinks-2.2.1.Linux_x86_64/cuffnorm --library-type=fr-firststrand /work/bc187/vivax_genome/PlasmoDB-32_PvivaxP01.gff ../*mal.cxb
+mv cuffnorm fpkm_mal
+
+~/programs/cufflinks-2.2.1.Linux_x86_64/cuffnorm --library-type=fr-firststrand /work/bc187/human_genome/Homo_sapiens/NCBI/GRCh38/Annotation/Genes.gencode/genes.gtf ../*hum.cxb
+mv cuffnorm fpkm_hum
