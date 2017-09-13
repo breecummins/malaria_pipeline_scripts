0b0fb44c754ae211c9e46a18f1068784f33db70d
Bree Cummins <breecummins@gmail.com>
2017-09-12 20:27:03 -0600

    fixed cuffnorm script to put output in a folder

--- cuffnorm_analysis.sh
+++ cuffnorm_analysis.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/bash
 
-~/programs/cufflinks-2.2.1.Linux_x86_64/cuffnorm --library-type=fr-firststrand /work/bc187/vivax_genome/PlasmoDB-32_PvivaxP01.gff *mal.cxb
-mv cuffnorm fpkm_mal
+# call in results folder with aggregated .cxb files
 
-~/programs/cufflinks-2.2.1.Linux_x86_64/cuffnorm --library-type=fr-firststrand /work/bc187/human_genome/Homo_sapiens/NCBI/GRCh38/Annotation/Genes.gencode/genes.gtf *hum.cxb
-mv cuffnorm fpkm_hum
+mkdir fpkm_mal
+cd fpkm_mal
+~/programs/cufflinks-2.2.1.Linux_x86_64/cuffnorm --library-type=fr-firststrand /work/bc187/vivax_genome/PlasmoDB-32_PvivaxP01.gff ../*mal.cxb
+
+cd ..
+mkdir fpkm_hum
+cd fpkm_hum
+~/programs/cufflinks-2.2.1.Linux_x86_64/cuffnorm --library-type=fr-firststrand /work/bc187/human_genome/Homo_sapiens/NCBI/GRCh38/Annotation/Genes.gencode/genes.gtf ../*hum.cxb
+
+cd ..
